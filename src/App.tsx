64bffc5fe4a2ce4d785abd6a66ba922df047786b
Patrick <p.nascimento@akrsistemas.com.br>
2026-04-10 17:05:55 -0300

    Estruturando Site

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { Heading } from './components/Heading';
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati iure
-        voluptatibus ad officia, illo voluptatem laborum minus, similique earum
-        numquam pariatur sapiente dolorem quia totam praesentium minima nulla
-        omnis sed.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
